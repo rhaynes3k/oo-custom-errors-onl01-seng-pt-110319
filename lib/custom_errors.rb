8503f9d811792caec12c388796d05054c507b858
Rommel Haynes <rhaynes3k@gmail.com>
2020-01-01 22:59:31 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,9 +7,17 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+      if person.class != Person
+          raise
+        PartnerError
+      else
+        person.partner = self
   end
 
+  class PartnerError < StandardError
+    
+  end
+  
 end
 
 beyonce = Person.new("Beyonce")
